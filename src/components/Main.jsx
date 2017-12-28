23f2fe87531c4c1c71f88d9d071ca8954ab827af
pnaumov <ecrofeg1@gmail.com>
2017-12-28 14:33:09 +0300

    Steps components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,101 +1,70 @@
 import React from 'react';
 import Stepper, { Step, StepLabel } from 'material-ui/Stepper';
 import Button from 'material-ui/Button';
-import Typography from 'material-ui/Typography';
 
-const getStepContent = step => {
-	switch (step) {
-		case 0:
-			return 'Select campaign settings...';
-		case 1:
-			return 'What is an ad group anyways?';
-		case 2:
-			return 'This is the bit I really care about!';
-		default:
-			return 'Unknown step';
-	}
-};
+import OrderSearching from './steps/OrderSearching';
+import PassengersChoosing from './steps/PassengersChoosing';
+import SeatsSelection from './steps/SeatsSelection';
+import Results from './steps/Results';
 
 class Main extends React.Component {
 	state = {
-		activeStep: 0,
-		skipped: new Set()
+		activeStep: 0
 	};
 
-	isStepSkipped(step) {
-		return this.state.skipped.has(step);
-	}
+	steps = [
+		<OrderSearching/>,
+		<PassengersChoosing/>,
+		<SeatsSelection/>,
+		<Results/>
+	];
+
+	stepsLabels = [
+		'Поиск заказа',
+		'Выбор пассажиров',
+		'Выбор мест',
+		'Посадочный талон'
+	];
 
 	handleNext = () => {
-		const { activeStep } = this.state;
-		let { skipped } = this.state;
-
-		if (this.isStepSkipped(activeStep)) {
-			skipped = new Set(skipped.values());
-			skipped.delete(activeStep);
-		}
-
 		this.setState({
-			activeStep: activeStep + 1,
-			skipped
+			activeStep: this.state.activeStep + 1
 		});
 	};
 
 	handleBack = () => {
-		const { activeStep } = this.state;
-
 		this.setState({
-			activeStep: activeStep - 1
+			activeStep: this.state.activeStep - 1
 		});
 	};
 
 	render() {
-		const steps = [
-			'Поиск заказа',
-			'Выбор пассажиров',
-			'Выбор мест',
-			'Посадочный талон'
-		];
 		const { activeStep } = this.state;
 
 		return (
 			<div>
 				<Stepper activeStep={activeStep}>
-					{steps.map((label, index) => {
-						const
-							props = {},
-							labelProps = {};
-
-						return (
-							<Step key={label} {...props}>
-								<StepLabel {...labelProps}>{label}</StepLabel>
-							</Step>
-						);
-					})}
+					{this.stepsLabels.map(label => (
+						<Step key={label}>
+							<StepLabel>{label}</StepLabel>
+						</Step>
+					))}
 				</Stepper>
+
 				<div>
-					{activeStep === steps.length ? (
+					<div>
+						{this.steps[activeStep]}
+
 						<div>
-							<Typography>
-								All steps completed - you&quot;re finished
-							</Typography>
+							<Button disabled={activeStep === 0} onClick={this.handleBack}>
+								Back
+							</Button>
+
+							{activeStep < this.steps.length - 1 ? <Button raised color="primary" onClick={this.handleNext}>
+								Next
+							</Button> : null}
 						</div>
-					) : (
-						<div>
-							<Typography>{getStepContent(activeStep)}</Typography>
-							<div>
-								<Button
-									disabled={activeStep === 0}
-									onClick={this.handleBack}
-								>
-									Back
-								</Button>
-								<Button raised color="primary" onClick={this.handleNext}>
-									{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-								</Button>
-							</div>
-						</div>
-					)}
+					</div>
 				</div>
 			</div>
 		);
