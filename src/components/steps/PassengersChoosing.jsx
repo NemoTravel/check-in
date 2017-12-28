e7b1310e81caf3f34d295881d6bfbb934e4ba6d0
pnaumov <ecrofeg1@gmail.com>
2017-12-28 15:23:21 +0300

    Step: passengers choosing

--- src/components/steps/PassengersChoosing.jsx
+++ src/components/steps/PassengersChoosing.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import {
+	FormControl,
+	FormGroup,
+	FormControlLabel
+} from 'material-ui/Form';
+import Checkbox from 'material-ui/Checkbox';
 
-export default () => {
-	return <div>Выбор пассажиров</div>;
-};
+class PassengersChoosing extends React.Component {
+	render() {
+		return (
+			<FormControl component="fieldset">
+				<FormGroup>
+					<FormControlLabel
+						control={
+							<Checkbox
+								value="gilad"
+							/>
+						}
+						label="Григорьев Олег"
+					/>
+
+					<FormControlLabel
+						control={
+							<Checkbox
+								value="jason"
+							/>
+						}
+						label="Григорьева Ольга"
+					/>
+				</FormGroup>
+			</FormControl>
+
+		);
+	}
+}
+
+export default PassengersChoosing;
